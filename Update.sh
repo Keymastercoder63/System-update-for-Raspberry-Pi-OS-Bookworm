52a3d684f448d8db83a750eec972bb5263ae115e
Keymastercoder63 <keymaster63gd@gmail.com>
2026-09-10 16:59:03 -0400

    Enhance Update.sh for comprehensive package updates
    Refactor update script to check for updates across apt, flatpak, npm, and pipx. Added conditional checks for existing updates.

--- Update.sh
+++ Update.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
-set -e
 echo "Checking for updates..."
-sudo apt update
+UPDATE=$(sudo apt update)
+UPDATES=$(npx npm-check-updates -u)
 
 echo "Updating System/Packages..."
-sudo apt full-upgrade
-
-echo "Cleaning leftovers..."
+echo "Updating apt packages..."
+if echo "$UPDATE" | grep "All packages are up to date"; then
+    echo "All packages are already up to date."
+else
+    echo "Updates found. Updating packages"
+    sudo apt full-upgrade -y
+fi
+echo "Done"
+echo "Updating flatpak packages..."
+sudo flatpak update
+echo "Done"
+echo "Updating npm packages..."
+if echo "$UPDATES" | grep -q "All dependencies match the latest package versions"; then
+    echo "All packages are already up to date."
+else
+    echo "Updates found. Updating package.json..."
+    npx npm-check-updates -u
+    echo "Installing updated packages..."
+    npm install
+    echo "Update complete."
+fi
+echo "Done"
+echo "Updating pipx packages..."
+sudo pipx upgrade-all
+echo "Cleaning up..."
 sudo apt autoclean
+sync
 
 echo "System update complete"
